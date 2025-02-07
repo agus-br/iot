38d9de0bef2fabf56d7ce213751bdc00617241e4
Agustín Balcázar <agus.b.reyes@gmail.com>
2025-02-06 19:46:24 -0600

    Se creó una versión simplificada de la interfaz de control de la bomba de agua

--- 4_Bomba-agua/bomba_agua/bomba_agua.ino
+++ 4_Bomba-agua/bomba_agua/bomba_agua.ino
@@ -6,10 +6,13 @@ const int red = 10; // LED rojo
 const int yellow = 9; // LED amarillo
 const int green = 8; // LED verde
 
+const int rele = 12; // Pin del relevador 
+const int pinTest = 13; // Pin de prueba 
+
 
 void setup() {
 
-  Serial.begin(9600); // 
+  Serial.begin(9600);  
   
   pinMode(echo, INPUT); // Establecemos pin 5 como entrada de datos o pin de lectura
   pinMode(trig, OUTPUT); // Establecemos pin 4 como salida 
@@ -18,23 +21,42 @@ void setup() {
   pinMode(yellow, OUTPUT); // Establecemos pin conectado al LED amarillo como pin de salida
   pinMode(green, OUTPUT); // Establecemos pin conectado al LED verde como pin de salida
 
+  pinMode(rele, OUTPUT); // Establecemos el pin del rele como salida
+  digitalWrite(rele, HIGH); // Que comience apagada
 }
 
 void loop() {
+
+
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == 'a')
+      digitalWrite(rele, LOW);
+
+    if (c == 'q')
+      digitalWrite(rele, HIGH);
+  }
+
   int distance = sonar();
 
-  if(distance >= 20){
-    digitalWrite(green, HIGH);  
-    digitalWrite(yellow, LOW);
-    digitalWrite(red, LOW);
-  }else if(distance >= 10){
-    digitalWrite(green, LOW);  
-    digitalWrite(yellow, HIGH);
-    digitalWrite(red, LOW);
-  }else{
+  if(distance >= 13){
     digitalWrite(green, LOW);  
     digitalWrite(yellow, LOW);
     digitalWrite(red, HIGH);
+
+  }else if(distance >= 6){
+    digitalWrite(green, LOW);  
+    digitalWrite(yellow, HIGH);
+    digitalWrite(red, LOW);
+  }else if(distance >= 2){
+    digitalWrite(green, HIGH);  
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+  }else{
+    digitalWrite(rele, HIGH);  
+    digitalWrite(green, HIGH);  
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
   }
   
 }
@@ -54,8 +76,9 @@ int sonar(){
   Serial.print("Tiempo: ");
   Serial.println(time);
   Serial.print("Distancia: ");
-  Serial.println(distance);
   */
+  delay(100);
+  //Serial.println(distance);
   
-  return distancia
+  return distance;
 }
